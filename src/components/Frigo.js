8421c2a20a3cc104d749a9395815fa97ca35c605
Benoit Briastre <benoit.briastre@gmail.com>
2019-04-18 20:42:08 +0200

    Fixation

--- src/components/Frigo.js
+++ src/components/Frigo.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Casserole from './casserole';
 import './Frigo.css';
 
 class Frigo extends Component {
@@ -23,7 +24,10 @@ class Frigo extends Component {
     const { eggs } = this.state;
     return (
       <div className="frigostyle">
-        {eggs.map((egg, index) => <img key={index} src={egg.image} alt="" />)}
+        {eggs.filter((item) => item.$loki === 1 || item.$loki === 4 || item.$loki === 16 || item.$loki === 23 || item.$loki === 32 || item.$loki === 38 || 
+          item.$loki === 53 || item.$loki === 54 || item.$loki === 58 || item.$loki === 66 ||
+           item.$loki === 76 || item.$loki === 79 || item.$loki === 80).map((egg, index) => <img key={index} src={egg.image} alt="" />)}
+        <Casserole />
       </div>
     );
   }
